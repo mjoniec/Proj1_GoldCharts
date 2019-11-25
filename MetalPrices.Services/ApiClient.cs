4836d49ab4d4fbc27188578debbf59cc433e8500
mjoniec <mart1n.joniec@gmail.com>
2019-11-25 05:13:33 +0100

    abstract api client

---
+++ MetalPrices.Services/ApiClient.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MetalPrices.Services
+{
+    public abstract class ApiClient
+    {
+        private readonly HttpClient _client;
+
+        public ApiClient()
+        {
+            _client = HttpClientFactory.Create();
+        }
+    }
+}
--- MetalPrices.Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MetalPrices.Services
-{
-    public class Class1
-    {
-    }
-}
