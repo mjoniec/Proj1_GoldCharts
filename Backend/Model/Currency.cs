274e0b681549b36ef6df5689d4261e4ba5d9d148
mjoniec <mart1n.joniec@gmail.com>
2020-11-07 17:14:49 +0100

    remove old project

--- Backend/Model/Currency.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Model
-{
-    public enum Currency
-    {
-        USD,
-        EUR,
-        AUD
-    }
-}
--- Backend/Model/Metal.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Model
-{
-    public enum Metal
-    {
-        Gold,
-        Silver
-    }
-}
