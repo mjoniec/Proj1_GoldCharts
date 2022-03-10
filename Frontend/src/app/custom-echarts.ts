b551d8b17a7d37829d530a8dd2bde3b412e12cc1
mjoniec <mart1n.joniec@gmail.com>
2022-03-10 10:31:14 +0100

    remove frontend after moving to separate project

--- Frontend/src/app/custom-echarts.ts
+++
@@ -1,7 +0,0 @@
-// Import the main module of echarts and export them.
-export * from 'echarts/src/echarts';
-
-// Import modules we need
-import 'echarts/src/chart/bar';
-import 'echarts/src/chart/pie';
-
